f763b8f8025394cdf955abc1332469831af7f7a0
Colin Willson <colinwillson@gmail.com>
2023-07-31 16:42:26 -0700

    troubleshooting test

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const mainStyle = {
 
 
 const IndexPage = (props) => {
-  console.log('process.env.GATSBY_TEST', process.env.GATSBY_TESTprocess.env.GATSBY_TEST);
+  console.log('process.env.GATSBY_TEST', process.env.GATSBY_TEST);
   console.log('process.env.TEST', process.env.TEST);
 
   const [overlayIsActive, setOverlayIsActive] = useState(false);
