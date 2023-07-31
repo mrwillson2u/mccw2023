97df4c86fad9349d044153fc79e21f1951a5670f
Colin Willson <colinwillson@gmail.com>
2023-07-31 13:16:31 -0700

    Troubleshooting production environment

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react"
 import "../index.sass"
 import InfoOverlay from "../InfoOverlay"
 import TdButton from "../TdButton"
-import RSVPOverlay from "../overlays/RSVPOverlay"
+// import RSVPOverlay from "../overlays/RSVPOverlay"
 // import { v4 as uuidv4 } from 'uuid';
 
 // const useState = React.useState;
