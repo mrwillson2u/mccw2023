efb3f63091700ee9f1317900fe40f855339b5dfc
Colin Willson <colinwillson@gmail.com>
2023-08-01 20:58:35 -0700

    Realized I put the fonts in the wrong place. Also added the other ones to see if it will work on mobile

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -4,6 +4,22 @@ export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <link
       rel="preload"
+      href="/fonts/Dalime.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/SignPainter-HouseScript-01.otf"
+      as="font"
+      type="font/otf"
+      crossOrigin="anonymous"
+      key="interFont"
+    />,
+    <link
+      rel="preload"
       href="/fonts/Alegreya-Regular.ttf"
       as="font"
       type="font/ttf"
