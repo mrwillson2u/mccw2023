b6ac2ddbcfca37c255b84ca2e6de24510dde1d13
Colin Willson <colinwillson@gmail.com>
2023-08-03 11:00:44 -0700

    Updated some text formatting on the events page. Also updated some font styling

--- src/overlays/EventsOverlay.js
+++ src/overlays/EventsOverlay.js
@@ -10,11 +10,13 @@ const EventsOverlay = (props) => {
     <div className="contentContainer">
       <h3>Schedule of Events</h3>
       
-      <h5>Welcome Party</h5>
-      <p><strong>Theme: </strong>Tailgate</p>
-      <p><strong>Attire: </strong>Casual</p>
-      <p><strong>Time: </strong>5:00 pm - 10:00 pm</p>
-      <p><strong>Location: </strong>Sonom Coast Villa</p>
+      <h4>Welcome Party</h4>
+      <p>
+        <strong>Theme: </strong>Tailgate<br/>
+        <strong>Attire: </strong>Casual<br/>
+        <strong>Time: </strong>5:00 pm - 10:00 pm<br/>
+        <strong>Location: </strong>Sonom Coast Villa
+        </p>
       <p>
         We’re celebrating Maddie’s country farm roots by having a tailgate party! 
         There will be a variety of fun games on the lawn for people to connect 
@@ -23,10 +25,12 @@ const EventsOverlay = (props) => {
         until dark, so come on out and enjoy some good food, fun, and fellowship!
       </p>
 
-      <h5>Ceremony</h5>
-      <p><strong>Attire: </strong>Be your best self!</p>
-      <p><strong>Time: </strong>TBD</p>
-      <p><strong>Location: </strong>Bodega Bay Trailhead</p>
+      <h4>Ceremony</h4>
+      <p>
+        <strong>Attire: </strong>Be your best self!<br/>
+        <strong>Time: </strong>TBD<br/>
+        <strong>Location: </strong>Bodega Bay Trailhead
+      </p>
       <p>
         We cordially invite you to join us for our exchange of vows, to be held on 
         the beautiful rocky coast of California. The ceremony will take place just 
@@ -38,10 +42,12 @@ const EventsOverlay = (props) => {
         The wedding will be an intimate affair with the people in our lives we feel helped us become who we are today
       </p>
 
-      <h5>Reception</h5>
-      <p><strong>Attire: </strong>Be your best self!</p>
-      <p><strong>Time: </strong>TBD</p>
-      <p><strong>Location: </strong>Sonoma Coasy Villa</p>
+      <h4>Reception</h4>
+      <p>
+        <strong>Attire: </strong>Be your best self!<br/>
+        <strong>Time: </strong>TBD<br/>
+        <strong>Location: </strong>Sonoma Coasy Villa
+        </p>
       <p>
         All there is left to do at this point it is celebrate our wedding with food, 
         dancing, and all you lovely people! We will have plenty of food and drinks, 
@@ -49,10 +55,12 @@ const EventsOverlay = (props) => {
         wait to see you there!
       </p>
 
-      <h5>Farewell Brunch</h5>
-      <p><strong>Attire: </strong>Casual</p>
-      <p><strong>Time: </strong>8:00 am - 10:30 am</p>
-      <p><strong>Location: </strong>Sonoma Coast Villa</p>
+      <h4>Farewell Brunch</h4>
+      <p>
+        <strong>Attire: </strong>Casual<br/>
+        <strong>Time: </strong>8:00 am - 10:30 am<br/>
+        <strong>Location: </strong>Sonoma Coast Villa
+      </p>
       <p>
         As the festivities come to an end, come say goodbye and let us thank you for celebrating with us!
       </p>
